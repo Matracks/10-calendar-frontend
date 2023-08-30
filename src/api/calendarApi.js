de474a57005b9750dd3c78d77ddeec9c593c3fcd
matracks <matiasmoran05@gmail.com>
2023-08-30 20:18:11 -0300

    Update

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { getEnvVariables } from "../helpers/getEnvVariables";
 
-const { VITE_API_URL } = getEnvVariables()
-
 const calendarApi = axios.create({
-    baseURL: VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 calendarApi.interceptors.request.use( config => {
